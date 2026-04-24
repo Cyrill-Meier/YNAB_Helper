3d8e5d6def95cef4aee2263747ad7678d73b1e5f
Cyrill Meier <meier.cyrill@gmail.com>
2026-04-24 16:45:57 +0200

    Increase version code

--- revolut_ynab_bot.py
+++ revolut_ynab_bot.py
@@ -54,7 +54,7 @@ import revolut_to_ynab as ynab
 # BUILD_SHA and BUILD_DATE are injected at Docker build time from GitHub Actions
 # (see Dockerfile ARGs). When running locally from source, they stay "dev".
 
-__version__ = "1.1.10"
+__version__ = "1.1.11"
 
 
 def get_version_info():
